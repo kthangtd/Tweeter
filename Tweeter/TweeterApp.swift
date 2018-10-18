2816b7c8ef76640db3b0dedaf7687f6d090a724f
kthangtd <kthangtd@gmail.com>
2018-10-19 00:24:30 +0700

    init project

---
+++ Tweeter/TweeterApp.swift
@@ -0,0 +1,21 @@
+//
+//  TweeterApp.swift
+//  Tweeter
+//
+//  Created by Ngo Thang Loi on 10/19/18.
+//  Copyright © 2018 kthangtd. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class TweeterApp: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
